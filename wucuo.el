1e552058b443711e773067a4f20826e65e0e2783
Chen Bin <chenbin.sh@gmail.com>
2018-09-07 16:38:29 +1000

    README

--- wucuo.el
+++ wucuo.el
@@ -33,6 +33,9 @@
 ;; Run `wucuo-start' to setup and start `flyspell-mode'.
 ;; It spell check camel case words in code.
 ;;
+;; Please note `flyspell-prog-mode' should not be enabled when using "wucuo".
+;; `flyspell-prog-mode' could be replaced by "wucuo".
+;;
 ;; OR add one line setup if you prefer running `flyspell-buffer' manually:
 ;;  (setq flyspell-generic-check-word-predicate #'wucuo-generic-check-word-predicate)
 ;;
