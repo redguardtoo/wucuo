399add0cafa80a5050b23412b64d82e6b9d2c996
Chen Bin <chenbin.sh@gmail.com>
2020-10-21 18:33:41 +1100

    add pkg.sh

---
+++ pkg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+name=wucuo
+version=0.2.8
+pkg=$name-$version
+mkdir $pkg
+cp *.el $pkg
+cat << EOF > $pkg/$name-pkg.el
+(define-package "$name" "$version"
+                "whatever")
+EOF
+if [[ `uname -s` == *Darwin* ]]; then
+   COPYFILE_DISABLE="" tar cvf $pkg.tar $pkg/
+else
+   tar cvf $pkg.tar $pkg/
+fi
+rm -rf $pkg/
